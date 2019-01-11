b15d73e39d37c7756d7a197f62d2e796ae8b24be
mxding <937652174@qq.com>
2019-01-11 18:32:29 +0800

    添加日志输出

--- src/main/java/net/ruixin/flow/repository/UserRepository.java
+++ src/main/java/net/ruixin/flow/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package net.ruixin.flow.repository;
 
+import net.ruixin.flow.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @Author: mxding
  * @Date: 2019-01-11 16:25
  */
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
--- src/test/java/repository/UserRepositoryTest.java
+++ src/test/java/repository/UserRepositoryTest.java
@@ -1,12 +1,15 @@
 package repository;
 
 import net.ruixin.flow.Application;
+import net.ruixin.flow.entity.User;
 import net.ruixin.flow.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @Author: mxding
@@ -22,5 +25,21 @@ public class UserRepositoryTest {
     @Test
     public void test() {
         System.out.println(userRepository);
+    }
+
+    @Test
+    public void testAddUser() {
+        User user = new User();
+        user.setName("Lily");
+        user.setPhone("57892");
+        userRepository.save(user);
+    }
+
+    @Test
+    public void testListUser() {
+        List<User> userList = userRepository.findAll();
+        userList.forEach(user -> {
+            System.out.println(user.getId() + "|" + user.getName() + "|" + user.getPhone());
+        });
     }
 }
