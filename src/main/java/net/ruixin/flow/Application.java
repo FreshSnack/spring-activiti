683acfefd598a71bd05581e21f4954692e704bd9
FreshSnack <937652174@qq.com>
2019-01-12 09:04:06 +0800

    配置activiti

--- src/main/java/net/ruixin/flow/Application.java
+++ src/main/java/net/ruixin/flow/Application.java
@@ -1,13 +1,16 @@
 package net.ruixin.flow;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: mxding
  * @Date: 2019-01-11 14:01
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
